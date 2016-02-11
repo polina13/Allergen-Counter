7a72df5e1f010e71209cef602634032b00f76cb1
Polina Nenchev and Ryan King <ryanking157@hotmail.com>
2016-02-11 15:36:51 -0800

    Edit readme file and add style to main page

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -18,7 +18,7 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
       goTo("http://localhost:4567/");
-      assertThat(pageSource()).contains("Title");
+      assertThat(pageSource()).contains("Allergy Counter");
   }
 
   @Test
